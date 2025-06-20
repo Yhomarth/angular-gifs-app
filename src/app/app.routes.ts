9163ab1e4b86d46a912803515295e165d5605ee9
Yhomarth Reyes <y.reyes@simv.gob.do>
2025-06-20 10:57:02 -0400

    pensando en componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    loadComponent: ()=> import( './gifs/pages/dashboard-app/dashboard-app.component' )
+  },
+
+  {
+    path : '**',
+    redirectTo: 'dashboard'
+  }
+
+
+
+];
---
+++ src/app/gifs/pages/dashboard-app/dashboard-app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-app',
+  imports: [],
+  templateUrl: './dashboard-app.component.html'
+})
+export default class DashboardAppComponent { }
