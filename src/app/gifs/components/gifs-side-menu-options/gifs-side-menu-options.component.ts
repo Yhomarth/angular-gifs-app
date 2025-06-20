0799295bb3e6cf09fcb777bdbf43d4596c5e4e63
Yhomarth Reyes <y.reyes@simv.gob.do>
2025-06-20 15:46:53 -0400

    RouterLink desde componentes hijos

--- src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ src/app/gifs/components/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MenuOption } from '../../interfaces/menu-options.interfaces';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-options.component.html',
-  styles: ``
 })
 export class GifsSideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon : 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs mas populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon : 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    }
+  ];
 
 }
