840da87970190250415b38e36fd3f861dfb67630
Yhomarth Reyes <y.reyes@simv.gob.do>
2025-06-20 14:57:55 -0400

    Componentes para el menu lateral

--- src/app/gifs/pages/dashboard-app/dashboard-app.component.ts
+++ src/app/gifs/pages/dashboard-app/dashboard-app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { GifsSideMenuComponent } from "../../components/gifs-side-menu/gifs-side-menu.component";
+
 @Component({
   selector: 'app-dashboard-app',
-  imports: [ RouterOutlet ],
+  imports: [RouterOutlet, GifsSideMenuComponent],
   templateUrl: './dashboard-app.component.html'
 })
 export default class DashboardAppComponent { }
